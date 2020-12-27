db08ccc0893f1190f7f9ae14b53ae966fc28b6d3
Kaleb Nieman <kaleb.nieman@gmail.com>
2020-12-26 23:13:06 -0600

    made the filter work.  Will work on part 2 tomorrow.

--- UFO_level_1/static/js/app.js
+++ UFO_level_1/static/js/app.js
@@ -7,7 +7,7 @@ var tableData = data;
 
 var $tbody = d3.select("tbody");
 var button = d3.select("#filter-btn");
-var inputFieldData = d3.select("#datetime");
+var inputFieldDate = d3.select("#datetime");
 var inputFieldCity = d3.select("#city");
 var columns = ["datetime", "city", "state", "country", "shape", "durationMinutes", "comments"];
 
@@ -26,11 +26,22 @@ addData(tableData);
 button.on("click", () => {
     d3.event.preventDefault();
 
+var inputDate = inputFieldDate.property("value").trim();
+
+var filterDate = tableData.filter(tableData => tableData.datetime === inputDate);
+
+$tbody.html("");
+
+let response = {
+    filterDate
+}
+
+if(response.filterDate.length !==0) {
+    addData(filterDate);
+}
+
+else {
+    $tbody.append("tr").append("td").text("No sightings here!");
+}
+
 });
-
-
-
-
-
-
-
